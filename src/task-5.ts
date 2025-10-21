cbe964ea3183776f2029da4d87ae6347f8a1b37f
dmytro_bykanov <dmitbykanov@gmail.com>
2025-10-21 19:48:13 +0300

    task 5

---
+++ src/task-5.ts
@@ -0,0 +1,13 @@
+type Status = "success" | "error" | "loading";
+
+function logStatus(status: Status): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong.");
+  }
+}
+
+logStatus("loading");
